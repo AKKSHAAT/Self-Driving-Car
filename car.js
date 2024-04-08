590c618cc6138b5ddc5faf724f68182f00c9f05b
AKKSHAAT <akkshaat@gmail.com>
2024-04-08 19:57:32 +0530

    added: cars control

--- car.js
+++ car.js
@@ -1,24 +1,36 @@
 class Car {
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
-        this.controls = new this.controls();
+    this.controls = new Controls();
+  }
+
+  update() {
+    if (this.controls.forward) {
+      this.y -= 2;
     }
-
-
-    draw(context){
-        context.beginPath();
-        context.rect(
-            this.x-this.width / 2,
-            this.y-this.width / 2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
+    if (this.controls.reverse) {
+      this.y += 2;
     }
+    if (this.controls.left) {
+      this.x -= 2;
+    }
+    if (this.controls.right) {
+      this.x += 2;
+    }
+  }
 
-     
+  draw(context) {
+    context.beginPath();
+    context.rect(
+      this.x - this.width / 2,
+      this.y - this.width / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+  }
 }
--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const canvas =document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
 
-canvas.height = window.innerHeight;
 canvas.window = 200;
-
 
 const ctx = canvas.getContext("2d");
 
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx)
+car.draw(ctx);
+
+animate();
+
+function animate() {
+  car.update();
+  canvas.height = window.innerHeight;  // also clears the animation
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
